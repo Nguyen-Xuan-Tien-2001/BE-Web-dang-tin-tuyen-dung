33f9f10f05b90562f88970d4195c15ffb5c311ab
Nguyen Xuan Tien <nguyenxuantien20062001@gmail.com>
2023-11-11 11:28:44 +0700

    fixAddRoleAdmin

--- src/main/java/com/example/Fiverr/FiverrApplication.java
+++ src/main/java/com/example/Fiverr/FiverrApplication.java
@@ -1,8 +1,17 @@
 package com.example.Fiverr;
 
+import java.util.HashSet;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.Fiverr.Model.RoleEnum;
+import com.example.Fiverr.Model.Users;
+import com.example.Fiverr.Repository.UserRepository;
 
 // @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 // @CrossOrigin(origins = "*")
@@ -12,5 +21,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FiverrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FiverrApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserRepository userRepository,
+			PasswordEncoder passwordEncoder) {
+		return args -> {
+			Users admin = new Users(1L, "tien20062001@gmail.com", "admin","123456" ,RoleEnum.ADMIN);
+
+			userRepository.save(admin);
+		};
 	}
 }
